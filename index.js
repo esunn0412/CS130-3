46295acb3f4ed4f9ea66ae581c5376344bfdee6f
TAEEUN KIM <taeeunk1208@gmail.com>
2026-02-11 15:51:34 -0500

    additional functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const operators = {
+  "+": (a, b) => a.plus(b),
+  "-": (a, b) => a.minus(b),
+  "*": (a, b) => a.times(b),
+  "/": (a, b) => a.div(b),
+};
+
 class Calculator {
   constructor() {
     this.displayElement = document.querySelector(".display");
@@ -29,10 +36,10 @@ class Calculator {
   }
 
   set operator(value) {
-    if (!this.firstOperand || this.operator || this.secondOperand) return;
+    if (!this.firstOperand) return;
 
+    this.isDecimalUsed = false;
     this._operator = value;
-    this._display = this.firstOperand + " " + value;
   }
 
   get operator() {
@@ -43,7 +50,7 @@ class Calculator {
     if (!this.firstOperand || !this.operator) return;
 
     this._secondOperand = value;
-    this.display = this.firstOperand + " " + this.operator + " " + value;
+    this.display = value;
   }
 
   get secondOperand() {
@@ -57,8 +64,68 @@ class Calculator {
     this.secondOperand = null;
     this.isDecimalUsed = false;
   }
+
+  back() {
+    if (this.display.length === 1) {
+      if (!this.operator) {
+        this.firstOperand = null;
+      } else {
+        this.secondOperand = null;
+      }
+      this.display = "0";
+    }
+
+    if (!this.operator) {
+      this.firstOperand = this.firstOperand.slice(0, -1); // slice is not in place
+      this.display = this.firstOperand;
+      return;
+    }
+
+    this.secondOperand = this.secondOperand.slice(0, -1);
+    this.display = this.secondOperand;
+  }
+
+  signSwitch() {
+    if (!this.operator) {
+      if (!this.firstOperand) return;
+
+      if (this.firstOperand.startsWith("-")) {
+        this.display = this.firstOperand.slice(1);
+      } else {
+        this.display = "-" + this.firstOperand;
+      }
+    }
+
+    if (!this.secondOperand) return;
+
+    if (this.secondOperand.startsWith("-")) {
+      this.display = this.secondOperand.slice(1);
+    } else {
+      this.display = "-" + this.secondOperand;
+    }
+  }
+
+  equals() {
+    if (!this.firstOperand || !this.operator || !this.secondOperand) return;
+
+    const a = parseFloat(this.firstOperand);
+    const b = parseFloat(this.secondOperand);
+
+    const first = Big(this.firstOperand);
+    const second = Big(this.secondOperand);
+
+    const operation = operators[this.operator];
+    const result = operation(first, second).toString();
+
+    this.display = result;
+
+    this.clear();
+    this.firstOperand = result;
+  }
 }
 
 const cal = new Calculator();
-cal.firstOperand = "23";
+cal.firstOperand = "1";
+cal.back();
+
 console.log(cal);
